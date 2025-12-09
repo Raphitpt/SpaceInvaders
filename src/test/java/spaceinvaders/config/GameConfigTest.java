cd9dc3ae83b64d04f527d9a02516310755994b78
POUPLIER Eliot <eliot.pouplier@maif.fr>
2025-12-09 09:15:02 +0100

    tu InputHandler

--- src/test/java/spaceinvaders/config/GameConfigTest.java
+++ src/test/java/spaceinvaders/config/GameConfigTest.java
@@ -2,7 +2,6 @@ package spaceinvaders.config;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import spaceinvaders.config.GameConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
